7f4dbc71bfd8bc0dd312790079a0793315df729b
Artefact <artefactgo@gmail.com>
2020-09-26 13:33:46 +0900

    スコープとロールを共有プロジェクトに切り出す

---
+++ ResourceOwnerPasswordCredentialGrant/Constants/RollTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace Constants
+{
+    public class RollTypes
+    {
+        public const string User = "User";
+        public const string Admin = "Admin";
+        public const string Uploader = "Uploader";
+    }
+}
---
+++ ResourceOwnerPasswordCredentialGrant/Constants/ScopeTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace Constants
+{
+    public class ScopeTypes
+    {
+        public const string Standard = "Standard";
+        public const string UploadOnly = "UploadOnly";
+    }
+}
